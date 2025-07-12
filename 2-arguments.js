c8dc414b4f926847aa5515b45f00265dfeff1547
Osinachi Aluche <osinachialuchevictor@gmail.com>
2025-07-12 09:04:45 +0100

    Add arguments

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2); // skip the first two default values
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
